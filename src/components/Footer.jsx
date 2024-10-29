931bbca81e6e051cf27862671ac6b427ff40006d
tanyavohra <vohratanya5@gmail.com>
2024-10-29 20:58:06 +0530

    Final commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
   return (
     <div>
       <div id="footer" >
-<a href="https://github.com/tanyavohra/portfolio.git">
+<a href="https://github.com/tanyavohra/MyPortfolio.git">
 <p style={{color:"white",fontFamily:"Montserrat",fontSize:20,marginTop:70,marginBottom:50}}>
       Made by : Tanya Vohra
 </p>
